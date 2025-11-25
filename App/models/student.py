d9401b0efe1394dcded5c39ccb8c7dfc5f1fa1fe
denisseee <serrettedeniss@gmail.com>
2025-11-25 12:51:50 -0400

    Edited Student model

--- App/models/student.py
+++ App/models/student.py
@@ -5,7 +5,6 @@ from datetime import date
 
 #edited
 class Student(db.Model):
-    __tablename__ = 'student'
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False, unique=True)
     username =  db.Column(db.String(20), nullable=False, unique=True)
